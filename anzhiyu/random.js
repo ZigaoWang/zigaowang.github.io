68a3558795b14464b813533cc204ca5ad6b04175
ZigaoWang <zigao.wang24@gmail.com>
2023-09-28 07:01:11 +0800

    Site updated: 2023-09-28 07:01:11

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["2023/06/10/","2022/12/20/","2023/08/31/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["2022/12/20/","2023/08/31/","2023/06/10/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
